f9bd84ef33a4480cf1e295f3ab5d32b98f4a9503
Prasad484 <prasadrahatekar4@gmail.com>
2021-12-19 22:45:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+function Daily() {
+    document.getElementById("WorkTime").innerText = "5hrs";
+    document.getElementById("WorkWeek").innerText = "Last Weak - 7hrs";
+
+
+    document.getElementById("PlayTime").innerText = "1hr";
+    document.getElementById("PlayWeek").innerText = "Last Weak - 2hrs";
+
+    
+    document.getElementById("StudyTime").innerText = "0hr";
+    document.getElementById("StudyWeek").innerText = "Last Weak - 1hr";
+
+
+      
+    document.getElementById("ExerciseTime").innerText = "1hr";
+    document.getElementById("ExerciseWeek").innerText = "Last Weak - 1hr";
+
+
+    document.getElementById("SocialTime").innerText = "1hr";
+    document.getElementById("SocialWeek").innerText = "Last Weak - 3hrs";
+
+
+    document.getElementById("SelfTime").innerText = "0hr";
+    document.getElementById("SelfWeek").innerText = "Last Weak - 1hr";
+
+
+}
+
+function Weekly() {
+    document.getElementById("WorkTime").innerText = "32hrs";
+    document.getElementById("WorkWeek").innerText = "Last Weak - 36hrs";
+
+    
+    document.getElementById("PlayTime").innerText = "10hrs";
+    document.getElementById("PlayWeek").innerText = "Last Weak - 8hrs";
+
+    
+    document.getElementById("StudyTime").innerText = "4hrs";
+    document.getElementById("StudyWeek").innerText = "Last Weak - 7hrs";
+
+
+    document.getElementById("ExerciseTime").innerText = "2hrs";
+    document.getElementById("ExerciseWeek").innerText = "Last Weak - 2hrs";
+
+
+    document.getElementById("SocialTime").innerText = "5hrs";
+    document.getElementById("SocialWeek").innerText = "Last Weak - 10hrs";
+
+    
+    document.getElementById("SelfTime").innerText = "1hr";
+    document.getElementById("SelfWeek").innerText = "Last Weak - 3hrs";
+
+}
+
+function Monthly() {
+    document.getElementById("WorkTime").innerText = "103hrs";
+    document.getElementById("WorkWeek").innerText = "Last Weak - 128hrs";
+
+    
+    document.getElementById("StudyTime").innerText = "13hrs";
+    document.getElementById("StudyWeek").innerText = "Last Weak - 19hrs";
+
+
+    document.getElementById("ExerciseTime").innerText = "10hr";
+    document.getElementById("ExerciseWeek").innerText = "Last Weak - 18hrs";
+
+
+    document.getElementById("SocialTime").innerText = "21hrs";
+    document.getElementById("SocialWeek").innerText = "Last Weak - 23hrs";
+
+    
+    document.getElementById("SelfTime").innerText = "7hrs";
+    document.getElementById("SelfWeek").innerText = "Last Weak - 11hrs";
+
+}
